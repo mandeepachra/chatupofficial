26cad7bd4adb8deaa131bc4215631f50f3d5cbaa
mandeepachra <deepdevils5@gmail.com>
2026-03-08 22:59:29 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,18 @@
-self.addEventListener('install', (e) => {
-  console.log('ChatUp Service Worker Installed');
+self.addEventListener('install', (e) => self.skipWaiting());
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(clients.openWindow('/index.html'));
 });
 
-self.addEventListener('fetch', (e) => {
-  // Filhal khali rakhenge taaki live data aaye
+self.addEventListener('push', function(event) {
+  const data = event.data ? event.data.text() : 'New notification on ChatUp';
+  event.waitUntil(
+    self.registration.showNotification('ChatUp', {
+      body: data,
+      icon: 'logo.png',
+      badge: 'logo.png',
+      vibrate: [200, 100, 200]
+    })
+  );
 });
